340995d32bef6b1eb4a7aa170b906d3c737c2664
katieywu <katiewu95@gmail.com>
2016-11-06 17:16:22 -0500

    made cool low poly look

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var light = new THREE.DirectionalLight( 0xefefff, 1.5 );
-				light.position.set( 1, 1, 1 ).normalize();
-				scene.add( light );
+//var light = new THREE.DirectionalLight( 0xefefff, 1.0 );
+//				light.position.set( 1, 1, 1 ).normalize();
+var light = new THREE.HemisphereLight( 0xffffdf, 0x080820, 1 );
+scene.add(light);
 
 
 var renderer = new THREE.WebGLRenderer();
@@ -11,16 +12,16 @@ renderer.setClearColor( 0xf0f0f0 );
 renderer.setPixelRatio( window.devicePixelRatio );
 document.body.appendChild( renderer.domElement );
 
-//var renderer = new THREE.CanvasRenderer();
-//renderer.setClearColor( 0xf0f0f0 );
-//renderer.setPixelRatio( window.devicePixelRatio );
-//renderer.setSize( window.innerWidth, window.innerHeight );
-//document.body.appendChild(renderer.domElement);
-
 var geometry = new THREE.IcosahedronGeometry(2, 0);
 
-//var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshPhongMaterial(
+    {color: 0xffdcb3, 
+     specular: 0x3bbbff,
+     shininess: 10,
+     shading: THREE.FlatShading, 
+     emissive: 0xff5757 } );
+
+
 var sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
 
@@ -33,8 +34,6 @@ var render = function () {
 
     sphere.rotation.x += 0.01;
     sphere.rotation.y += 0.01;
-    sphere.rotation.z += 0.01;
-
 
     renderer.render(scene, camera);
 };
